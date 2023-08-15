9d89cffd2aee51f699df9581919f237d34d3e76a
Dominik <dotto@fredhutch.org>
2023-08-15 13:48:35 -0700

    python test script

---
+++ scripts/test.sh
@@ -0,0 +1,37 @@
+root=/fh/fast/setty_m/user/dotto/benchmarkDA
+meld_bin=$root/methods/meld/bm_meld.py
+cna_bin="$root/methods/cna/bm_cna.py"
+data_id="covid19-pbmc"
+data_dir=${root}/data/real/$data_id
+pop=CD14_Monocyte
+batch_sd=0
+k=30
+resolution=0.5
+beta=25
+downsample=3
+mem=32g
+seed=43
+pop_enr=0.75
+
+cmd="python $meld_bin \
+--data_dir ${data_dir} \
+--data_id ${data_id} \
+--pop_enr $pop_enr \
+--k $k \
+--beta ${beta} \
+--pop ${pop} \
+--be_sd $batch_sd \
+--seed $seed \
+--outdir ${root}/benchmark/${data_id}/"
+
+cmd="python $cna_bin \
+--data_dir ${data_dir} \
+--data_id ${data_id} \
+--pop_enr $pop_enr \
+--k $k \
+--pop ${pop} \
+--be_sd $batch_sd \
+--seed $seed \
+--outdir ${root}/benchmark/${data_id}/"
+echo "$cmd"
+eval "$cmd"
