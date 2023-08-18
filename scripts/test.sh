717da01ac94612cab957721180f342bea3a6f066
Dominik <dotto@fredhutch.org>
2023-08-18 14:39:07 -0700

    add results notebook

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,13 @@ meld_bin=$root/methods/meld/bm_meld.py
 cna_bin="$root/methods/cna/bm_cna.py"
 mellon_bin=$root/methods/mellon/bm_mellon.py
 data_id="bcr-xl"
-data_id="covid19-pbmc"
-data_id="cluster"
+#data_id="covid19-pbmc"
+#data_id="cluster"
 data_dir=${root}/data/real/$data_id
-data_dir=${root}/data/synthetic/$data_id
+#data_dir=${root}/data/synthetic/$data_id
 pop=CD14_Monocyte
 pop=M1
+pop=CD4_T-cells
 batch_sd=0
 k=30
 resolution=0.5
@@ -17,7 +18,7 @@ downsample=3
 mem=32g
 seed=43
 pop_enr=0.75
-method=cydar
+method=milo
 
 cmd="python $meld_bin \
 --data_dir ${data_dir} \
@@ -30,24 +31,24 @@ cmd="python $meld_bin \
 --seed $seed \
 --outdir ${root}/benchmark/${data_id}/"
 
-cmd="python $cna_bin \
---data_dir ${data_dir} \
---data_id ${data_id} \
---pop_enr $pop_enr \
---k $k \
---pop ${pop} \
---be_sd $batch_sd \
---seed $seed \
---outdir ${root}/benchmark/${data_id}/"
-
-cmd="python $mellon_bin \
---data_dir ${data_dir} \
---data_id ${data_id} \
---pop_enr $pop_enr \
---pop ${pop} \
---be_sd $batch_sd \
---seed $seed \
---outdir ${root}/benchmark/${data_id}/"
+#cmd="python $cna_bin \
+#--data_dir ${data_dir} \
+#--data_id ${data_id} \
+#--pop_enr $pop_enr \
+#--k $k \
+#--pop ${pop} \
+#--be_sd $batch_sd \
+#--seed $seed \
+#--outdir ${root}/benchmark/${data_id}/"
+#
+#cmd="python $mellon_bin \
+#--data_dir ${data_dir} \
+#--data_id ${data_id} \
+#--pop_enr $pop_enr \
+#--pop ${pop} \
+#--be_sd $batch_sd \
+#--seed $seed \
+#--outdir ${root}/benchmark/${data_id}/"
 
 echo "$cmd"
-eval "$cmd"
+#eval "$cmd"
